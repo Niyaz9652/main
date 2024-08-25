fc6f33228d223ceae9fae0525bbb3ae2acd7e6aa
Darzi Niyaz Ahammad <niyazdarzi@gmail.com>
2024-08-25 10:52:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import { About } from "./Components/About";
 import { Home } from "./Components/Home";
 function App() {
   return (
-   <Routes>
+    <div className="App">
+      <h1>Niyaz</h1>
+   <Router>
+    <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="about" element={<About/>}/>
    </Routes>
+   </Router>
+   </div>
   );
 }
 
